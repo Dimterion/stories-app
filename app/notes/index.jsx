00f735aa5f0e668a0736ed61cb46c7d3e89d3281
Dmitrii <dimterion59@gmail.com>
2025-03-21 17:11:48 +0100

    add noNotes text

--- app/notes/index.jsx
+++ app/notes/index.jsx
@@ -115,7 +115,11 @@ const NoteScreen = () => {
       ) : (
         <>
           {error && <Text style={styles.errorText}>{error}</Text>}
-          <NoteList notes={notes} onDelete={deleteNote} onEdit={editNote} />
+          {notes.length === 0 ? (
+            <Text style={styles.noNotesText}>You have no notes</Text>
+          ) : (
+            <NoteList notes={notes} onDelete={deleteNote} onEdit={editNote} />
+          )}
         </>
       )}
       <TouchableOpacity
@@ -161,6 +165,13 @@ const styles = StyleSheet.create({
     textAlign: "center",
     marginBottom: 10,
     fontSize: 16,
+  },
+  noNotesText: {
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#555",
+    marginTop: 15,
   },
 });
 
