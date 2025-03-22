1d342d25562b2f16c1292fd50705d539cf3cb0d5
Dmitrii <dimterion59@gmail.com>
2025-03-22 15:41:46 +0100

    add about screen initial content

--- app/about/index.jsx
+++ app/about/index.jsx
@@ -1,11 +1,35 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 const AboutScreen = () => {
   return (
-    <View>
-      <Text>About screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Stories app</Text>
+      <Text style={styles.subtitle}>About stories app</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "#f8f9fa",
+    gap: 10,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 10,
+    color: "#333",
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "#666",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+});
 
 export default AboutScreen;
