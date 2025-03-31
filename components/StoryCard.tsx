a595f31f3fa44827cac195c44b4e5f1b3e5156b3
Dmitrii <dimterion59@gmail.com>
2025-03-31 12:15:31 +0200

    adjust StoryCard img

--- components/StoryCard.tsx
+++ components/StoryCard.tsx
@@ -1,12 +1,13 @@
 import { Image, Text, TouchableOpacity } from "react-native";
 import { Link } from "expo-router";
+import defaultImg from "@/assets/images/logo.png";
 
 const StoryCard = ({ id, title, cover_img }: Story) => {
   return (
     <Link href={`/story/${id}`} asChild>
       <TouchableOpacity className="w-[30%]">
         <Image
-          source={cover_img}
+          source={cover_img || defaultImg}
           className="w-full h-52 max-w-[20vw] max-h-[20vw] rounded-lg"
           resizeMode="cover"
         />
