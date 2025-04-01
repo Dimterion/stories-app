14b7c3cc06622feaa641ad9bd95a30e14a9917df
Dmitrii <dimterion59@gmail.com>
2025-04-01 12:04:27 +0200

    adjust search screen

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,9 +1,31 @@
-import { View, Text } from "react-native";
+import { FlatList, Image, View } from "react-native";
+import { images } from "@/constants/images";
+import { stories } from "@/assets/stories";
+import StoryCard from "@/components/StoryCard";
 
 const Search = () => {
   return (
-    <View>
-      <Text>Search</Text>
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="flex-1 absolute w-full z-0"
+        resizeMode="cover"
+      />
+      <FlatList
+        data={stories}
+        renderItem={({ item }) => <StoryCard {...item} />}
+        keyExtractor={(item) => item.id.toString()}
+        className="px-5"
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "center",
+          gap: 16,
+          marginVertical: 16,
+        }}
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+      />
     </View>
   );
 };
