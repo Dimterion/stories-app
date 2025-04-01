88d78fe1c0bc76e7bd5307a53192ddec1e4d0d45
Dmitrii <dimterion59@gmail.com>
2025-04-01 11:37:01 +0200

    adjust StoryCard component

--- components/StoryCard.tsx
+++ components/StoryCard.tsx
@@ -18,7 +18,9 @@ const StoryCard = ({
           className="w-full h-52 max-w-[20vw] max-h-[20vw] rounded-lg"
           resizeMode="cover"
         />
-        <Text className="text-sm font-bold text-white mt-2">{title}</Text>
+        <Text className="text-sm font-bold text-white mt-2" numberOfLines={1}>
+          {title}
+        </Text>
         <View className="flex-row items-center justify-start gap-x-1">
           <Image source={icons.star} className="size-4" />
           <Text className="text-xs text-white font-bold uppercase">
@@ -27,6 +29,9 @@ const StoryCard = ({
           <View className="flex-flow items-center justify-between">
             <Text className="text-xs text-light-300 font-medium mt-1">
               {publish_date?.split("-")[0]}
+            </Text>
+            <Text className="text-xs font-medium text-light-300 uppercase">
+              Story
             </Text>
           </View>
         </View>
