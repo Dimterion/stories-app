90699009258ec211574f7eeb9764892889d188d4
Dmitrii <dimterion59@gmail.com>
2025-04-04 10:55:01 +0200

    adjust profile screen

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,11 @@
-import { View, Text } from "react-native";
+import { Image, Text, View } from "react-native";
+import { icons } from "@/constants/icons";
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View className="bg-primary flex-1 px-10 flex justify-center items-center flex-col gap-5">
+      <Image source={icons.person} className="size-10" tintColor="#fff" />
+      <Text className="text-gray-500 text-base">Profile</Text>
     </View>
   );
 };
