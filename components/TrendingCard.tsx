ac3cde6e3daa54ecbf4be015895d9b80c0ba211e
Dmitrii <dimterion59@gmail.com>
2025-04-05 12:19:55 +0200

    adjust TrendingCard elements

--- components/TrendingCard.tsx
+++ components/TrendingCard.tsx
@@ -1,5 +1,6 @@
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import { Link } from "expo-router";
+import { images } from "@/constants/images";
 
 const TrendingCard = ({
   story: { story_id, title, cover_img },
@@ -13,7 +14,14 @@ const TrendingCard = ({
           className="w-32 h-48 rounded-lg"
           resizeMode="cover"
         />
-        <View className="absolute bottom-9 -left-3.5 px-2 py-1 rounded-full"></View>
+        <View className="absolute bottom-9 -left-3.5 px-2 py-1 rounded-full">
+          <Image
+            source={images.rankingGradient}
+            className="size-1"
+            resizeMode="cover"
+          />
+          <Text className="text-white text-6xl">{index + 1}</Text>
+        </View>
       </TouchableOpacity>
     </Link>
   );
