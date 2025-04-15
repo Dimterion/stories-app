63a3c81e558e01e9b81a2c1fede6801fdaf1ce1c
Dmitrii <dimterion59@gmail.com>
2025-04-15 11:25:38 +0200

    adjust StoryCard elements style

--- components/StoryCard.tsx
+++ components/StoryCard.tsx
@@ -11,7 +11,11 @@ const StoryCard = ({
   release_date,
 }: Story) => {
   return (
-    <Link href={`/stories/${id}`} asChild>
+    <Link
+      href={`/stories/${id}`}
+      asChild
+      className="border-2 border-tertiary rounded-md bg-accent flex items-center"
+    >
       <TouchableOpacity className="w-[30%]">
         <Image
           source={{
@@ -19,21 +23,24 @@ const StoryCard = ({
               ? `${process.env.EXPO_PUBLIC_POSTER_IMG}${poster_path}`
               : process.env.EXPO_PUBLIC_PLACEHOLDER_IMG,
           }}
-          className="w-full h-52 rounded-lg"
+          className="w-full h-52 border-b-2 border-tertiary rounded-t"
           resizeMode="cover"
         />
-        <Text className="text-sm font-bold text-white mt-2" numberOfLines={1}>
+        <Text
+          className="text-sm font-bold text-tertiary mt-2"
+          numberOfLines={1}
+        >
           {title}
         </Text>
         <View className="flex-row items-center justify-start gap-x-1">
           <Image source={icons.star} className="size-4" />
-          <Text className="text-xs text-white font-bold uppercase">
+          <Text className="text-xs text-tertiary font-bold uppercase">
             {Math.round(vote_average / 2)}
           </Text>
         </View>
 
         <View className="flex-row items-center justify-between">
-          <Text className="text-xs text-light-300 font-medium mt-1">
+          <Text className="text-xs text-tertiary font-medium mt-1">
             {release_date?.split("-")[0]}
           </Text>
         </View>
