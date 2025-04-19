5ff3d49b4880d10ce9f8c8fdd9017226eb8caa65
Dmitrii <dimterion59@gmail.com>
2025-04-19 12:10:36 +0200

    adjust ind story screen

--- app/stories/[id].tsx
+++ app/stories/[id].tsx
@@ -42,46 +42,12 @@ const StoryDetailsScreen = () => {
         </View>
 
         <View className="flex-col items-start justify-center mt-5 px-5">
-          <Text className="text-tertiary font-bold text-xl">
+          <Text className="text-tertiary font-bold text-4xl mx-auto">
             {story?.title}
           </Text>
-          <View className="flex-row items-center gap-x-1 mt-2">
-            <Text className="text-tertiary text-sm">
-              {story?.release_date?.split("-")[0]} •
-            </Text>
-            <Text className="text-tertiary text-sm">{story?.runtime}m</Text>
-          </View>
-
-          <View className="flex-row items-center bg-dark-100 px-2 py-1 rounded-md gap-x-1 mt-2">
-            <Image source={icons.star} className="size-4" />
-
-            <Text className="text-white font-bold text-sm">
-              {Math.round(story?.vote_average ?? 0)}/10
-            </Text>
-
-            <Text className="text-white text-sm">
-              ({story?.vote_count} votes)
-            </Text>
-          </View>
 
           <StoryInfo label="Overview" value={story?.overview} />
-          <StoryInfo
-            label="Genres"
-            value={story?.genres?.map((g) => g.name).join(" • ") || "N/A"}
-          />
 
-          <View className="flex flex-row justify-between w-1/2">
-            <StoryInfo
-              label="Budget"
-              value={`$${(story?.budget ?? 0) / 1_000_000} million`}
-            />
-            <StoryInfo
-              label="Revenue"
-              value={`$${Math.round(
-                (story?.revenue ?? 0) / 1_000_000
-              )} million`}
-            />
-          </View>
           <StoryInfo
             label="Production Companies"
             value={
--- components/StoryInfo.tsx
+++ components/StoryInfo.tsx
@@ -2,10 +2,8 @@ import { Text, View } from "react-native";
 
 const StoryInfo = ({ label, value }: StoryInfoProps) => (
   <View className="flex-col items-start justify-center mt-5">
-    <Text className="text-tertiary font-normal text-sm">{label}</Text>
-    <Text className="text-tertiary font-bold text-sm mt-2">
-      {value || "N/A"}
-    </Text>
+    <Text className="text-tertiary font-bold text-sm">{label}</Text>
+    <Text className="text-tertiary text-sm mt-2">{value || "N/A"}</Text>
   </View>
 );
 
