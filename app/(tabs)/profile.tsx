613bb4e8655ebd657945c787b391a0354cd582e9
Dmitrii <dimterion59@gmail.com>
2025-04-20 10:25:10 +0200

    adjust profile screen elements

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -5,6 +5,8 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { icons } from "@/constants/icons";
 import Header from "@/components/Header";
 
+const links = ["X", "G", "M", "I", "W", "E"];
+
 const ProfileScreen = () => {
   return (
     <SafeAreaView className="bg-primary flex-1 px-10">
@@ -28,42 +30,34 @@ const ProfileScreen = () => {
         <Text className="text-tertiary text-2xl text-center font-bold">
           Contact Links
         </Text>
-        <View className="flex flex-row gap-4 justify-center my-4">
-          <Link
-            href={`/`}
-            asChild
-            className="border-2 border-tertiary rounded-md bg-accent flex items-center shadow-md shadow-tertiary w-4 py-2 px-4"
-          >
-            <TouchableOpacity>
-              <Text>X</Text>
-            </TouchableOpacity>
-          </Link>
-          <Link
-            href={`/`}
-            asChild
-            className="border-2 border-tertiary rounded-md bg-accent flex items-center shadow-md shadow-tertiary w-4 py-2 px-4"
-          >
-            <TouchableOpacity>
-              <Text>X</Text>
-            </TouchableOpacity>
-          </Link>
-          <Link
-            href={`/`}
-            asChild
-            className="border-2 border-tertiary rounded-md bg-accent flex items-center shadow-md shadow-tertiary w-4 py-2 px-4"
-          >
-            <TouchableOpacity>
-              <Text>X</Text>
-            </TouchableOpacity>
-          </Link>
+        <View className="flex flex-row flex-wrap gap-4 justify-center my-4">
+          {links.map((link) => (
+            <Link
+              href={`/`}
+              asChild
+              className="border-2 border-tertiary rounded-md bg-accent flex items-center shadow-md shadow-tertiary w-4 py-2 px-4"
+            >
+              <TouchableOpacity>
+                <Text>{link}</Text>
+              </TouchableOpacity>
+            </Link>
+          ))}
         </View>
       </View>
       <View className="flex items-center gap-4">
-        <Image
-          source={icons.person}
-          className="border-2 border-tertiary rounded-full"
-          tintColor="#fff"
-        />
+        <Link
+          href={`/`}
+          asChild
+          className="border-2 border-tertiary rounded-md bg-accent flex items-center shadow-md shadow-tertiary w-4 py-2 px-4"
+        >
+          <TouchableOpacity>
+            <Image
+              source={icons.person}
+              className="border-2 border-tertiary rounded-full"
+              tintColor="#fff"
+            />
+          </TouchableOpacity>
+        </Link>
         <Text>Check the latest story</Text>
       </View>
     </SafeAreaView>
