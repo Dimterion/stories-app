81351e969b7e898a0686c119bfbf862a8706ae91
Dmitrii <dimterion59@gmail.com>
2025-04-20 10:20:58 +0200

    adjust profile screen elements

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -24,23 +24,47 @@ const ProfileScreen = () => {
           tintColor="#fff"
         />
       </View>
-      <View>
-        <View>
-          <Text className="text-tertiary text-2xl text-center font-bold">
-            Contact Links
-          </Text>
-          <View className="mx-4">
-            <Link
-              href={`/`}
-              asChild
-              className="border-2 border-tertiary rounded-md bg-accent flex items-center shadow-md shadow-tertiary w-4 py-2 px-4"
-            >
-              <TouchableOpacity>
-                <Text>X</Text>
-              </TouchableOpacity>
-            </Link>
-          </View>
+      <View className="border-b-2 border-tertiary m-4 py-4">
+        <Text className="text-tertiary text-2xl text-center font-bold">
+          Contact Links
+        </Text>
+        <View className="flex flex-row gap-4 justify-center my-4">
+          <Link
+            href={`/`}
+            asChild
+            className="border-2 border-tertiary rounded-md bg-accent flex items-center shadow-md shadow-tertiary w-4 py-2 px-4"
+          >
+            <TouchableOpacity>
+              <Text>X</Text>
+            </TouchableOpacity>
+          </Link>
+          <Link
+            href={`/`}
+            asChild
+            className="border-2 border-tertiary rounded-md bg-accent flex items-center shadow-md shadow-tertiary w-4 py-2 px-4"
+          >
+            <TouchableOpacity>
+              <Text>X</Text>
+            </TouchableOpacity>
+          </Link>
+          <Link
+            href={`/`}
+            asChild
+            className="border-2 border-tertiary rounded-md bg-accent flex items-center shadow-md shadow-tertiary w-4 py-2 px-4"
+          >
+            <TouchableOpacity>
+              <Text>X</Text>
+            </TouchableOpacity>
+          </Link>
         </View>
+      </View>
+      <View className="flex items-center gap-4">
+        <Image
+          source={icons.person}
+          className="border-2 border-tertiary rounded-full"
+          tintColor="#fff"
+        />
+        <Text>Check the latest story</Text>
       </View>
     </SafeAreaView>
   );
