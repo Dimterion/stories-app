7251c5486a90774f233d35253954b92a0030bfa5
Dmitrii <dimterion59@gmail.com>
2025-05-15 12:25:26 +0200

    remove unused img

--- constants/images.ts
+++ constants/images.ts
@@ -1,9 +1,7 @@
-import bg from "@/assets/images/bg.png";
 import profilePic from "@/assets/images/profile_pic.png";
 import placeholderImg from "@/assets/images/placeholder_img.png";
 
 export const images = {
-  bg,
   profilePic,
   placeholderImg,
 };
