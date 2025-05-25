9f90ffc6efc4e75f92da7d5b043e47734af209f6
Dmitrii <dimterion59@gmail.com>
2025-05-25 10:34:07 +0200

    adjust formatText function

--- utils/utils.tsx
+++ utils/utils.tsx
@@ -1,20 +1,42 @@
-import { Text } from "react-native";
+import { StyleSheet, Text } from "react-native";
 
 export const formatText = (text: string) => {
-  const parts = text.split(/(\*.*?\*|_.*?_)/);
+  const parts = text.split(/(\*\*.*?\*\*|__.*?__|_[^_]*?_|\*.*?\*)/g);
+  const textStyles = StyleSheet.create({
+    bold: {
+      fontWeight: "bold",
+    },
+    italic: {
+      fontStyle: "italic",
+    },
+  });
+
+  if (!text) {
+    return null;
+  }
 
   return parts.map((part, index) => {
-    if (part.startsWith("*") && part.endsWith("*")) {
+    if (
+      part.startsWith("**") &&
+      part.endsWith("**") &&
+      part.length > 2 &&
+      part !== "**"
+    ) {
       return (
-        <Text key={index} style={{ fontWeight: "bold" }}>
-          {part.slice(1, -1)}
+        <Text key={index} style={textStyles.bold}>
+          {part.slice(2, -2)}
         </Text>
       );
     }
 
-    if (part.startsWith("_") && part.endsWith("_")) {
+    if (
+      part.startsWith("*") &&
+      part.endsWith("*") &&
+      part.length > 1 &&
+      part !== "**"
+    ) {
       return (
-        <Text key={index} style={{ fontStyle: "italic" }}>
+        <Text key={index} style={textStyles.italic}>
           {part.slice(1, -1)}
         </Text>
       );
